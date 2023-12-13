d74296a00aceb8d194d56ea9ba42dc6769bbf0e6
RainLin05 <rainl5@nycstudents.net>
2023-12-13 15:04:36 -0500

    e

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
 async function getData() {
-    const response = await fetch("2ColumnDataSet.csv");
+    const response = await fetch("ZonAnn.Ts+dSST.csv");
     const data = await response.text();
     const rows = data.split("\n").slice(1);
+    const list = [];
     rows.forEach((elem) => {
               const row = elem.split(",");
               const year = row[0];
               const temp = row[1];
-              console.log(year, temp);
+              list[elem] = {x: year, y: temp}
             });
-          }
+    return list
+    }
     //end of functions
     //call the function to test if you see the image on the left in the console
     {
